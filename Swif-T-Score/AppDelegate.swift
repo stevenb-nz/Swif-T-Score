7d2c4f7851feefe4204d286a03aea9570677a76e
Steven Brown <sgbrown@mac.com>
2019-07-30 22:43:46 +1200

    Initial Commit

---
+++ Swif-T-Score/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swif-T-Score
+//
+//  Created by Steven Brown on 30/07/19.
+//  Copyright © 2019 Steven Brown. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swif-T-ScoreTests/Swif_T_ScoreTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swif_T_ScoreTests.swift
+//  Swif-T-ScoreTests
+//
+//  Created by Steven Brown on 30/07/19.
+//  Copyright © 2019 Steven Brown. All rights reserved.
+//
+
+import XCTest
+@testable import Swif_T_Score
+
+class Swif_T_ScoreTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
